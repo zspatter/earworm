ba5c4a9f0973c50b5a022c80385b2385a45b5b01
zspatter <localhost@zspatter.dev>
2019-08-01 09:45:07 -0400

    adjusts directory structure of project
    this places the library and script in a child directory to separate these files from config files (gitignore, readme, requirements, etc.)

---
+++ send_earworms/send_earworm.py
@@ -0,0 +1,124 @@
+import random
+from os import environ
+from pathlib import Path
+
+import lyricsgenius
+import openpyxl
+from pyshorteners import Shortener, Shorteners
+from twilio.rest import Client
+
+
+def get_clients():
+    """
+    Initializes genius client and twilio client using environment variables
+    """
+    account_sid = environ.get('TWILIO_ACCOUNT_SID')
+    auth_token = environ.get('TWILIO_AUTH_TOKEN')
+    genius_token = environ.get('GENIUS_TOKEN')
+
+    genius = lyricsgenius.Genius(genius_token)
+    twilio = Client(account_sid, auth_token)
+
+    return genius, twilio
+
+
+def get_earworm(sheet):
+    """
+    Chooses random song from the library and returns the title, artist, and earworm
+    
+    :param Worksheet sheet: worksheet containing earworm library
+    """
+    row = random.randint(2, sheet.max_row)
+    title = sheet.cell(row=row, column=1).value.strip()
+    artist = sheet.cell(row=row, column=2).value.strip()
+    earworm = sheet.cell(row=row, column=3).value.strip()
+
+    return title, artist, earworm
+
+
+def get_genius_link(genius, title, artist):
+    """
+    Uses Genius client to search for the song via artist and title query.
+    Once a match is found, the url for the result is returned
+
+    :param Genius genius: genius client (genius.com)
+    :param str title: name of song
+    :param str artist: name of artist
+    """
+    return genius.search_song(title=title, artist=artist).url
+
+
+def shorten_link(long_url, access_token):
+    """
+    Takes a long url and shortens it using pyshorteners (this implementation
+    is for bitly, but pyshorteners supports other shorteners as well)
+
+    :param str long_url: full link to shorten
+    :param dict access_token: access token for bitly
+    """
+    shortener = Shortener(Shorteners.BITLY, **access_token)
+
+    return shortener.short(long_url)
+
+
+def send_sms(client, message, recipient):
+    """
+    Using the provided twilio client, the given message is transmitted
+    to the recipient via SMS
+
+    :param Client client: twilio client used to send message
+    :param str message: message body
+    :param str recipient: recipient's phone number
+    """
+    sender = environ.get('TWILIO_NUMBER')
+    client.messages.create(body=message, from_=sender, to=recipient)
+
+
+def build_message(lyrics, url, emoji='🎶🎵🎶'):
+    """
+    Builds a message body by padding either side of the earworm lyrics with
+    emojis and appending a link to the lyrics to the end.
+
+    :param str lyrics: earworm (catchy lyrics)
+    :param str url: url to song's lyrics on genius
+    :param str emoji: emojis to pad earworm text with
+    """
+    return f'{emoji}' \
+        f'\n{lyrics}' \
+        f'\n{emoji}' \
+        f'\n{url}'
+
+
+def send_earworm(sheet, genius, access_token, twilio, recipient):
+    """
+    Using the provided earworm library, a random song is chosen. Then, the genius
+    client gathers the url to the lyrics for said song. After the full link is
+    retrieved, the link is shortened using bitly. Finally, the earworm message
+    is created and sent to the given recipient over SMS via the twilio client
+
+    :param Worksheet sheet: library of send_earworms
+    :param Genius genius: genius client (genius.com)
+    :param dict access_token: access token for bitly
+    :param Client twilio: twilio client used to send message
+    :param str recipient: recipient's phone number
+    """
+    song_title, song_artist, earworm_lyrics = get_earworm(sheet)
+    original_url = get_genius_link(genius=genius,
+                                   title=song_title,
+                                   artist=song_artist)
+    short_url = shorten_link(long_url=original_url, access_token=access_token)
+    earworm_message = build_message(lyrics=earworm_lyrics, url=short_url)
+    send_sms(client=twilio, message=earworm_message, recipient=recipient)
+
+
+if __name__ == '__main__':
+    bitly_token = {'bitly_token': environ.get('BITLY_TOKEN')}
+    genius_client, twilio_client = get_clients()
+
+    wb = openpyxl.load_workbook(Path('./earworm_library/earworms.xlsx'))
+    ws = wb.active
+    send_earworm(sheet=ws,
+                 genius=genius_client,
+                 access_token=bitly_token,
+                 twilio=twilio_client,
+                 recipient=environ.get('RECIPIENT'))
