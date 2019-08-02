5760ddbdf291f71adbee6122f19af9672e2f0cd6
zspatter <localhost@zspatter.dev>
2019-08-02 15:04:13 -0400

    adds scheduling at random intervals and checks for availability window

--- send_earworms/send_earworm.py
+++ send_earworms/send_earworm.py
@@ -1,14 +1,30 @@
 import logging
 import random
-from datetime import datetime
+from datetime import datetime, time
 from os import environ
 from pathlib import Path
+from time import sleep
 
 import lyricsgenius
 import openpyxl
+import schedule
 from pyshorteners import Shortener, Shorteners
 from pytz import timezone, utc
 from twilio.rest import Client
+
+
+def logger_setup():
+    """
+    Sets up logger with specified format and explictly converts time to EDT
+    regardless of local timezone
+    """
+    logging.basicConfig(filename=f'{Path(__file__).stem}.log',
+                        level=logging.INFO,
+                        format=' %(asctime)s.%(msecs)03d - %(levelname)s - '
+                               '<%(funcName)s>: %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    logging.Formatter.converter = custom_time
+    logging.getLogger("twilio").setLevel(logging.WARNING)
 
 
 def get_clients():
@@ -23,7 +39,51 @@ def get_clients():
     twilio = Client(account_sid, auth_token)
     logging.debug(f'Clients for Genius and Twilio created')
 
-    return genius, twilio
+
+def schedule_earworms(lower_bound, upper_bound):
+    """
+    Schedules recurring tasks at random intervals between the provided bounds
+
+    :param int lower_bound: lower bound of interval (in minutes)
+    :param int upper_bound: upper bound of interval (in minutes)
+    """
+    schedule.every(lower_bound).to(upper_bound).minutes.do(send_earworm,
+                                                           sheet=ws,
+                                                           genius=genius_client,
+                                                           access_token=bitly_token,
+                                                           twilio=twilio_client,
+                                                           recipient=environ.get('RECIPIENT'))
+    while True:
+        try:
+            if is_available():
+                schedule.run_pending()
+            sleep(30)
+        except Exception as e:
+            logging.exception(e)
+
+
+def send_earworm(sheet, genius, access_token, twilio, recipient):
+    """
+    Using the provided earworm library, a random song is chosen. Then, the genius
+    client gathers the url to the lyrics for said song. After the full link is
+    retrieved, the link is shortened using bitly. Finally, the earworm message
+    is created and sent to the given recipient over SMS via the twilio client
+
+    :param Worksheet sheet: library of send_earworms
+    :param Genius genius: genius client (genius.com)
+    :param dict access_token: access token for bitly
+    :param Client twilio: twilio client used to send message
+    :param str recipient: recipient's phone number
+    """
+    # if is_available():
+    logging.info(f'Gathering earworm')
+    song_title, song_artist, earworm_lyrics = get_earworm(sheet)
+    original_url = get_genius_link(genius=genius,
+                                   title=song_title,
+                                   artist=song_artist)
+    short_url = shorten_link(long_url=original_url, access_token=access_token)
+    earworm_message = build_message(lyrics=earworm_lyrics, url=short_url)
+    send_sms(client=twilio, message=earworm_message, recipient=recipient)
 
 
 def get_earworm(sheet):
@@ -99,55 +159,40 @@ def send_sms(client, message, recipient):
     logging.info(f'Earworm sent to recipient')
 
 
-def send_earworm(sheet, genius, access_token, twilio, recipient):
+def get_edt_time():
     """
-    Using the provided earworm library, a random song is chosen. Then, the genius
-    client gathers the url to the lyrics for said song. After the full link is
-    retrieved, the link is shortened using bitly. Finally, the earworm message
-    is created and sent to the given recipient over SMS via the twilio client
-
-    :param Worksheet sheet: library of send_earworms
-    :param Genius genius: genius client (genius.com)
-    :param dict access_token: access token for bitly
-    :param Client twilio: twilio client used to send message
-    :param str recipient: recipient's phone number
-    """
-    logging.info(f'Gathering earworm')
-    song_title, song_artist, earworm_lyrics = get_earworm(sheet)
-    original_url = get_genius_link(genius=genius,
-                                   title=song_title,
-                                   artist=song_artist)
-    short_url = shorten_link(long_url=original_url, access_token=access_token)
-    earworm_message = build_message(lyrics=earworm_lyrics, url=short_url)
-    send_sms(client=twilio, message=earworm_message, recipient=recipient)
-
-
-def edt_time(*args):
-    """
-    Converts time to EDT (regardless of local time)
+    Returns current time in EDT
     """
     utc_dt = utc.localize(datetime.utcnow())
     timezone_ = timezone('US/Eastern')
-    converted_time = utc_dt.astimezone(tz=timezone_)
-    return converted_time.timetuple()
+    return utc_dt.astimezone(tz=timezone_)
+
+
+def is_available():
+    """
+    Checks if current time is within availability range
+    """
+    # return 9 <= get_edt_time().hour <= 23
+    start = time(hour=9, minute=0, second=0, tzinfo=timezone('US/Eastern'))
+    end = time(hour=23, minute=0, second=0, tzinfo=timezone('US/Eastern'))
+    return start <= get_edt_time().time() <= end
+
+
+def custom_time(*args):
+    """
+    Converts local time to custom timezone (EDT)
+    """
+    return get_edt_time().timetuple()
+
+    return genius, twilio
 
 
 if __name__ == '__main__':
-    logging.basicConfig(filename=f'{Path(__file__).stem}.log',
-                        level=logging.INFO,
-                        format=' %(asctime)s.%(msecs)03d - %(levelname)s - '
-                               '<%(funcName)s>: %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
-    logging.Formatter.converter = edt_time
-    logging.getLogger("twilio").setLevel(logging.WARNING)
-
+    logger_setup()
     bitly_token = {'bitly_token': environ.get('BITLY_TOKEN')}
     genius_client, twilio_client = get_clients()
 
     wb = openpyxl.load_workbook(Path('./earworm_library/earworms.xlsx'))
     ws = wb.active
-    send_earworm(sheet=ws,
-                 genius=genius_client,
-                 access_token=bitly_token,
-                 twilio=twilio_client,
-                 recipient=environ.get('RECIPIENT'))
+
+    schedule_earworms(lower_bound=90, upper_bound=4 * 60)
